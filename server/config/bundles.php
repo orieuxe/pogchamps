c3b740fbe92f9077015aa075e23e81b461fa78e1
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 16:11:45 +0200

    makerbundle for dev only

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -6,5 +6,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'prod' => false],
 ];
