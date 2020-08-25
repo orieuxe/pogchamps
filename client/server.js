de68d7a0341487cb664b60ab7433e20fc2968bcb
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 23:08:25 +0200

    allow cors attempt

--- client/server.js
+++ client/server.js
@@ -7,7 +7,22 @@ const path = require('path');
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 // Serve only the static files form the dist directory
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/dist/pogchamps'));
 
 app.get('/*', function(req,res) {
