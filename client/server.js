fe24e20e6caac78b261c55d8fe347018c8882d67
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 23:15:09 +0200

    allow cors attempt

--- client/server.js
+++ client/server.js
@@ -9,8 +9,8 @@ const app = express();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
@@ -21,8 +21,8 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
-// Serve only the static files form the dist directory
 app.use(allowCrossDomain);
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pogchamps'));
 
 app.get('/*', function(req,res) {
