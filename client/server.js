d86bf5b91871e9b6f4759f5ca0f5e732a7a608c3
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 23:35:00 +0200

    cleaner cors

--- client/server.js
+++ client/server.js
@@ -8,19 +8,11 @@ const path = require('path');
 const app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-   res.header("Access-Control-Allow-Credentials", true);
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-   next();
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 };
 
 app.use(allowCrossDomain);
