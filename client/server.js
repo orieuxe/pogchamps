bf4c6ba7a3448817250c8fd1b3e21f33d607b546
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 19:02:53 +0200

    add dist folder for express server

--- client/server.js
+++ client/server.js
@@ -8,10 +8,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/pogchamps'));
+app.use(express.static(__dirname + '/dist/pogchamps'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/pogchamps/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/pogchamps/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
