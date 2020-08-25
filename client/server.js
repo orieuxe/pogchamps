de1e372119432f38b219020e3199c0da610c5441
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 23:25:10 +0200

    even bigger headers allowed

--- client/server.js
+++ client/server.js
@@ -8,9 +8,11 @@ const path = require('path');
 const app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   next();
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
