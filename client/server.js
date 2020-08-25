772445796e26050aa12d3f424b17e30d41cfa68b
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-25 18:32:00 +0200

    add express server for angular heroku deploiment

---
+++ client/server.js
@@ -0,0 +1,18 @@
+//Only required for deploiement on heroku
+
+//Install express server
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
