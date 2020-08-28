418362f0c4fb5f3f703fa7e7db9551a30967b31d
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-28 16:14:07 +0200

    cleaner python scripts section

--- server/python-scripts/script.sh
+++ server/python-scripts/script.sh
@@ -3,6 +3,7 @@ d=$(date +%Y-%m-%d)
 mv ~/Downloads/chess_com_games_$d.pgn pgn/$db.pgn
 python pgn-to-sql.py pgn/$db.pgn > sql/$db.sql
 sed -i 'N;$!P;D' sql/$db.sql
-python3 sql-to-db.py $db
-cd ../../
+url=$(heroku config:get DATABASE_URL -a apichamps)
+python3 sql-to-db.py $db $url
+cd ../
 php bin/console app:update
---
+++ server/python-scripts/sql-to-db.py
@@ -0,0 +1,24 @@
+import psycopg2
+import sys
+from urllib.parse import urlparse
+
+file_name = sys.argv[1]
+db_url = sys.argv[2]
+
+result = urlparse(db_url);
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+conn = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname
+)
+
+curs = conn.cursor();
+
+sql_file = open('sql/'+file_name+'.sql')
+curs.execute(sql_file.read())
+conn.commit();
