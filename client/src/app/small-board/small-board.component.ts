eabac469596bbf4fa841a8a0bc8002bdf6847fcb
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-08-29 18:47:28 +0200

    small improvements on game list

--- client/src/app/small-board/small-board.component.ts
+++ client/src/app/small-board/small-board.component.ts
@@ -33,6 +33,7 @@ export class SmallBoardComponent implements OnInit {
   setTimeControl(timecontrol : string){
     let minSec = timecontrol.split("+").map(Number);
     minSec[0] = minSec[0]/60;
+    if(minSec.length < 2) minSec.push(0);
     return minSec.join("+")
   }
 
