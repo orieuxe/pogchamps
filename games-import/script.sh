67cbc22cf56aac39a34eb1acb1440c3d2f5c8aff
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-09-04 20:24:35 +0200

    move python/shell scripts folder to root

--- games-import/script.sh
+++ games-import/script.sh
@@ -5,5 +5,5 @@ python pgn-to-sql.py pgn/$db.pgn > sql/$db.sql
 sed -i 'N;$!P;D' sql/$db.sql
 url=$(heroku config:get DATABASE_URL -a apichamps)
 python3 sql-to-db.py $db $url
-cd ../
+cd server/
 php bin/console app:update
