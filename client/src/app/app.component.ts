0d32d6c2430306e515a849773f22cbe587578e9c
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-12-06 14:44:09 +0100

    dark theme : storing user's choice to localStorage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', './theme.switch.css']
 })
-export class AppComponent {
-  title = 'pogchamps';
+export class AppComponent implements OnInit{
+  darkTheme: boolean;
+  private dataThemeAttribute = 'data-theme';
 
-  switchTheme(dark: boolean){
-    document.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
+  ngOnInit(): void {
+    const darkThemeStr = localStorage.getItem(this.dataThemeAttribute) || 'false'
+    this.darkTheme = darkThemeStr === 'true';
+    this.updateTheme(this.darkTheme);
+  }
+  
+  updateTheme(dark: boolean){
+    document.documentElement.setAttribute(this.dataThemeAttribute, dark ? 'dark' : 'light');
+    localStorage.setItem(this.dataThemeAttribute, dark.toString());
   }
 }
