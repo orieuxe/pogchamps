b1e7015d52e91de401e9ad3f9adb422199858e23
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-12-10 00:51:46 +0100

    argument required when importing to prod database

--- games-import/script.sh
+++ games-import/script.sh
@@ -3,8 +3,12 @@ d=$(date +%Y-%m-%d)
 mv ~/Downloads/chess_com_games_$d.pgn pgn/$db.pgn
 python pgn-to-sql.py pgn/$db.pgn > sql/$db.sql
 sed -i 'N;$!P;D' sql/$db.sql
-# url=$(heroku config:get DATABASE_URL -a apichamps)
-url="postgresql://:poggers@127.0.0.1:5432/$db"
+if [ $# -eq 0 ]
+  then
+    url="postgresql://:poggers@127.0.0.1:5432/$db"
+  else
+    url=$(heroku config:get DATABASE_URL -a apichamps)
+fi
 python3 sql-to-db.py $db $url
 cd ../server/
 php bin/console app:update
