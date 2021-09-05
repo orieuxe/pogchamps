8fe1bd7ddf41b0bb079f9e920f695e99d310cd43
orieuxe <orieux.etienne@gmail.com>
2021-09-05 23:43:02 +0200

    prevent 404 infinite loop if wrong tournament id

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useGlobal } from 'reactn';
+import { getTournamentCount, getTournamentIds } from '@services/TournamentService'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useGlobal } from 'reactn'
 
 export default function Custom404() {
-  const router = useRouter();
-  const [selectedTournament] = useGlobal('selectedTournament')
+	const router = useRouter()
+	const [selectedTournament, setSelectedTournament] = useGlobal('selectedTournament')
+	const tournamentIds = getTournamentIds()
+  const tournamentCount = getTournamentCount()
 
-  useEffect(() => {
-    router.replace(`/participants/${selectedTournament}`);
-  });
+	useEffect(() => {
+		const tournamentId = tournamentIds.includes(selectedTournament) ? selectedTournament : tournamentCount
+		setSelectedTournament(tournamentId)
+		router.replace(`/participants/${tournamentId}`)
+	}, [selectedTournament])
 
-  return null;
+	return null
 }
